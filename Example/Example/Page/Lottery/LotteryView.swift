9ac73676629f6e220b265bd68aeffb659d93ec3f
derek.y@tc-gaming.com <>
2021-06-18 17:05:04 +0800

    [Add] GeometryReader

--- Example/Example/Page/Lottery/LotteryView.swift
+++ Example/Example/Page/Lottery/LotteryView.swift
@@ -11,32 +11,43 @@ import SwiftUI
 struct LotteryView: View {
     
     @ObservedObject var control = LotteryControl()
-    
+    var radius: CGFloat = 100
+    var title: String = ""
     let colors = [Color.red, .black, .gray, .green, .blue, .orange, .yellow, .purple]
     
     var body: some View {
-        ZStack {
-            ForEach(0..<control.index, id: \.self) { idx in
-                Path { path in
-                    path.move(to: CGPoint(x: 150, y: 150))
-                    path.addArc(center: CGPoint(x: 150, y: 150),
-                                radius: 150,
-                                startAngle: Angle(degrees: Double(idx) * self.control.angle),
-                                endAngle: Angle(degrees: Double(idx+1) * self.control.angle),
-                                clockwise: false)
-                    path.addLine(to: CGPoint(x: 150, y: 150))
-                }.fill(self.colors[idx%self.colors.count])
-            }
-        }.frame(width: 300, height: 300,
-                alignment: .center)
-            .rotationEffect(.degrees(control.rotation))
+        GeometryReader { geometry in
+            VStack {
+                Text(title).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            ZStack {
+                ForEach(0..<control.index, id: \.self) { idx in
+                    Path { path in
+                        path.move(to: CGPoint(x: geometry.size.width/2, y: geometry.size.height/2))
+                        path.addArc(center: CGPoint(x: geometry.size.width/2, y: geometry.size.height/2),
+                                    radius: self.radius,
+                                    startAngle: Angle(degrees: Double(idx) * self.control.angle),
+                                    endAngle: Angle(degrees: Double(idx+1) * self.control.angle),
+                                    clockwise: false)
+                        path.addLine(to:  CGPoint(x: geometry.size.width/2, y: geometry.size.height/2))
+                    }.fill(self.colors[idx%self.colors.count])
+                }
+            }.frame(width: geometry.size.width, height: geometry.size.height,
+                    alignment: .center)
+                .rotationEffect(.degrees(control.rotation))
+        }
+        }
     }
 }
 
 struct LotteryView_Previews: PreviewProvider {
     
     static var previews: some View {
-        LotteryView().frame(width: 300, height: 300, alignment: .center)
+        LotteryView(radius: 150, title: "Wahaha")
+            .frame(width: 350, height: 350, alignment: .topLeading)
+            .background(Color.yellow.opacity(0.8))
+            .frame(width: 400, height: 200)
+            .background(Color.orange.opacity(0.5))
+        
     }
 }
 
